51d7fa45dd5096860e3a37ceeea36a9d88c9d559
m.shafie <shafieshahar1994@gmail.com>
2024-12-21 12:21:28 +0800

    Initial commit

--- app/src/main/java/com/shafie/sharedpreferencedemo/MainActivity.kt
+++ app/src/main/java/com/shafie/sharedpreferencedemo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.shafie.sharedpreferencedemo
 
+import android.app.backup.BackupManager
 import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.shafie.sharedpreferencedemo.databinding.ActivityMainBinding
 
@@ -9,12 +11,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private final val SAVED = "saved"
+    private var backupManager: BackupManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        backupManager = BackupManager(this)
 
         var savedData = loadPreference()
         if (savedData != "") {
@@ -26,6 +30,9 @@ class MainActivity : AppCompatActivity() {
 
         binding.saveDataBtn.setOnClickListener {
             val text = binding.saveDataEditText.text.toString()
+
+            Log.d("debug", "Calling Backup Manager")
+            backupManager?.dataChanged()
             if (text.isNotEmpty()) {
                 savePreference(text)
             }
@@ -39,14 +46,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun savePreference(value: String) {
-        val sharedPreferences = getPreferences(MODE_PRIVATE)
+        val sharedPreferences = getSharedPreferences(BackupData.PREFS_TEST, MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         editor.putString(SAVED, value)
         editor.apply() // Use apply() instead of commit() for asynchronous saving
     }
 
     private fun loadPreference(): String? {
-        val sharedPreferences = getPreferences(MODE_PRIVATE)
+        val sharedPreferences = getSharedPreferences(BackupData.PREFS_TEST,MODE_PRIVATE)
         return sharedPreferences.getString(SAVED, "")
     }
 }
